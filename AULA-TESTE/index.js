eb4f06169022a331cedb192339bf88230e46f5a9
Bernardo_Melo <be20super@gmail.com>
2025-08-17 22:26:46 -0300

    terceiro commit

--- AULA-TESTE/index.js
+++ AULA-TESTE/index.js
@@ -1,1 +1,31 @@
-console.log('olá!');
+let nomeDoHeroi = "Johnny Cage";
+let xpDoHeroi = 9500;
+ 
+let nivel;
+
+if (xpDoHeroi <= 1000) {
+    nivel = "Iniciante";
+}
+else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000){
+    nivel = "Bronze";
+}
+else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000){
+    nivel = "Prata";
+}
+else if (xpDoHeroi >= 5001 && xpDoHeroi <= 7000){
+    nivel = "Ouro";
+}
+else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000){
+    nivel = "Platina";
+}
+else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000){
+    nivel = "Ascendente";
+}
+else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000){
+    nivel = "Imortal";
+}
+else {
+    nivel = "Radiante";
+}
+
+console.log (`O Herói de nome ${nomeDoHeroi} está no nível ${nivel}`);
